596cd79706e656f7d7fb0d36308d9ecebfc86fc5
DrGooseman <jamesjquinn12@gmail.com>
2021-03-15 21:47:35 +0300

    add TCP server connection using java.net.Socket

--- API/src/main/java/Api.java
+++ API/src/main/java/Api.java
@@ -1,20 +1,37 @@
 import static spark.Spark.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 import services.EventService;
 
+/**
+ * The main class that will start the API to receive Events and send them out 
+ * to another server for storage using Google Protobuf.
+ * @author James
+ *
+ */
 public class Api {
 	public static void main(String[] args) {
 		Api api = new Api();
 		
+		api.setupServerConnection("localhost", 5000);
 		api.setupRoutes();
 	}
 	
 	private EventService eventService;
+	private Socket socket;
 
 	public Api() {
 		eventService = new EventService();
 	}
 
+	/**
+	 * Sets up routes using the Spark framework on http://localhost:4567/
+	 */
 	public void setupRoutes() {
 		post("/event", (req, res) -> {
 			res.type("application/json");
@@ -42,4 +59,31 @@ public class Api {
 		System.out.println("API listening on http://localhost:4567/");
 	}
 	
+	/**
+	 * Establishes a TCP socket connection to a server at the provided address:port 
+	 * on a new thread and listens for responses.
+	 * @param address
+	 * @param port
+	 */
+	public void setupServerConnection(String address, int port) {
+		Thread thread = new Thread() {
+			public void run() {
+				try (Socket newSocket = new Socket(address, port)) {
+					socket = newSocket;
+					BufferedReader echoes = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					eventService.setOutputStream(socket.getOutputStream());
+
+					while (true) {
+						String response = echoes.readLine();
+						System.out.println(response);
+					}
+				} catch (SocketTimeoutException e) {
+					System.out.println("The socket timed out!");
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		};
+		thread.start();
+	}
 }
