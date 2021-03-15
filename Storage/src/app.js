928d4445e8e846c8a599891f037e6854a5b1fb86
DrGooseman <jamesjquinn12@gmail.com>
2021-03-15 23:34:20 +0300

    add 'Storage', a basic TCP node.js server

---
+++ Storage/src/app.js
@@ -0,0 +1,31 @@
+const net = require("net");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const server = net.createServer();
+
+server.on("connection", (conn) => {
+  console.log("Connection established.");
+  conn.write("Connection established.\n");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  conn.once("close", () => {
+    console.log("Connection closed.");
+    conn.write("Connection closed.\n");
+  });
+
+  conn.on("error", (err) => {
+    console.log("Connection error. " + err.message);
+  });
+});
+
+const port = process.env.SERVER_PORT || 5000;
+const address = process.env.SERVER_ADDRESS || "127.0.0.1";
+
+server.listen(port, address, () => {
+  console.log(`server started at ${address}:${port}`);
+});
